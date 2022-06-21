62b624c8f23afa3a32cfcbb44d16eaea328f115b
Maxence Rogerieux <maxence.rogerieux@isen-ouest.yncrea.fr>
2022-06-21 15:11:27 +0200

    modification formulaire

--- js/updatePerson.js
+++ js/updatePerson.js
@@ -1,17 +1,14 @@
-// $('#update').on('click', (event) =>
-// {
-//     let id_photo = $('input[name="avatarInput"]:checked').val();
-//     let id_city = $('#townInput').val();
-//     let email = $('#mailInput').val();
-//     let age = $('#ageInput').val();
-//     let password = $('#passwordInput').val();
-//     event.preventDefault();
-//     ajaxRequest('PUT', '../php/libraries/updatePerson.php',displaySuccess,'id_photo=' + id_photo + '&id_city=' + id_city + '&email=' + email + '&password=' + password);
-// });
-//
-// function displaySuccess(){
-//     console.log("request create success");
-// }
+$('#updateForm').on('submit', (event) =>
+{
+    let id_photo = $('input[name="avatarInput"]:checked').val();
+    let id_city = $('#townInput').val();
+    let email = $('#mailInput').val();
+    let age = $('#ageInput').val();
+    let password = $('#passwordInput').val();
+    event.preventDefault();
+    ajaxRequest('PUT', '../php/libraries/updatePerson.php',displaySuccess,'id_photo=' + id_photo + '&id_city=' + id_city + '&email=' + email + '&password=' + password);
+});
 
-
-
+function displaySuccess(){
+    console.log("request create success");
+}
