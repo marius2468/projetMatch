70dd5419f1f4cce0e0394674c899b05a0a0b3c84
Maxence Rogerieux <maxence.rogerieux@isen-ouest.yncrea.fr>
2022-06-21 14:11:16 +0200

    modification js inscription

--- js/createPerson.js
+++ js/createPerson.js
@@ -2,15 +2,16 @@ $('#signUp').on('click', (event) =>
 {
     if ($('input[name="avatarInput"]:checked').val()!==null && $('#townInput').val()!==null && $('#mailInput').val()!==null && $('#ageInput').val()!==null && $('#passwordInput').val()!==null){
         let id_photo = $('input[name="avatarInput"]:checked').val();
+        let name = $('#nameInput').val();
+        let first_name = $('#firstNameInput').val();
         let id_city = $('#townInput').val();
         let email = $('#mailInput').val();
-        let age = $('#ageInput').val();
         let password = $('#passwordInput').val();
         event.preventDefault();
-        ajaxRequest('PUT', '../php/libraries/createPerson.php',displaySuccess,'id_photo=' + id_photo + '&name=' + name + '&first_name=' + first_name + '&id_city=' + id_city + '&email=' + email + '&password=' + password);
+        ajaxRequest('POST', '../php/libraries/createPerson.php',displaySuccess,'id_photo=' + id_photo + '&name=' + name + '&first_name=' + first_name + '&id_city=' + id_city + '&email=' + email + '&password=' + password);
     }
 });
 
 function displaySuccess(){
-    console.log("request update success");
+    console.log("request create success");
 }
--- js/updatePerson.js
+++ js/updatePerson.js
@@ -1,17 +1,17 @@
-$('#signUp').on('click', (event) =>
-{
-    let id_photo = $('input[name="avatarInput"]:checked').val();
-    let id_city = $('#townInput').val();
-    let email = $('#mailInput').val();
-    let age = $('#ageInput').val();
-    let password = $('#passwordInput').val();
-    event.preventDefault();
-    ajaxRequest('PUT', '../php/libraries/updatePerson.php',displaySuccess,'id_photo=' + id_photo + '&id_city=' + id_city + '&email=' + email + '&password=' + password);
-});
-
-function displaySuccess(){
-    console.log("request create success");
-}
+// $('#update').on('click', (event) =>
+// {
+//     let id_photo = $('input[name="avatarInput"]:checked').val();
+//     let id_city = $('#townInput').val();
+//     let email = $('#mailInput').val();
+//     let age = $('#ageInput').val();
+//     let password = $('#passwordInput').val();
+//     event.preventDefault();
+//     ajaxRequest('PUT', '../php/libraries/updatePerson.php',displaySuccess,'id_photo=' + id_photo + '&id_city=' + id_city + '&email=' + email + '&password=' + password);
+// });
+//
+// function displaySuccess(){
+//     console.log("request create success");
+// }
 
 
 
