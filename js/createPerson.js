07e710a36e093e7e13fe026e776e1613cff9bab6
Maxence Rogerieux <maxence.rogerieux@isen-ouest.yncrea.fr>
2022-06-21 14:05:42 +0200

    modification js inscription

--- js/createPerson.js
+++ js/createPerson.js
@@ -1,6 +1,6 @@
 $('#signUp').on('click', (event) =>
 {
-    if (!empty($data['first_name']) && !empty($data['name']) && !empty($data['email']) && !empty($data['password']) && !empty($data['id_photo']) && !empty($data['id_city'])){
+    if ($('input[name="avatarInput"]:checked').val()!==null && $('#townInput').val()!==null && $('#mailInput').val()!==null && $('#ageInput').val()!==null && $('#passwordInput').val()!==null){
         let id_photo = $('input[name="avatarInput"]:checked').val();
         let id_city = $('#townInput').val();
         let email = $('#mailInput').val();
@@ -9,8 +9,6 @@ $('#signUp').on('click', (event) =>
         event.preventDefault();
         ajaxRequest('PUT', '../php/libraries/createPerson.php',displaySuccess,'id_photo=' + id_photo + '&name=' + name + '&first_name=' + first_name + '&id_city=' + id_city + '&email=' + email + '&password=' + password);
     }
-
-
 });
 
 function displaySuccess(){
