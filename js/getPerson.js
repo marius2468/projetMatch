ee798b69e0b548dcaf1c4ff8eb0fc49c84d58ce8
Maxence Rogerieux <maxence.rogerieux@isen-ouest.yncrea.fr>
2022-06-22 12:04:01 +0200

    finalisation affichage profile

--- js/getPerson.js
+++ js/getPerson.js
@@ -1,23 +1,3 @@
-$('#signUpForm').on('submit', (event) =>
-{
-    let session;
-    ajaxRequest('GET', '../php/libraries/Person/getPerson.php?' + session,displayProfile);
-
-
-    let id_photo = 0;
-    let name = 0;
-    let first_name = 0;
-    let id_city = 0;
-    let age =0;
-    let email = 0;
-    let password = 0;
-    let id_physical_form = 0;
-    let application_note = 0;
-
-    event.preventDefault();
-});
-
-
 function $_GET(param) {
     var vars = {};
     window.location.href.replace( location.hash, '' ).replace(
@@ -33,57 +13,73 @@ function $_GET(param) {
     return vars;
 }
 
-let id_person = $_GET('id_person')
+// let id_person = $_GET('id_person')
+let id_person = 993;
 
 ajaxRequest('GET', '../php/libraries/Person/getPerson.php?id_person=' + id_person, displayPerson);
 
 function displayPerson(person){
+    let displayNote =  '';
+    for (let i = 1; i <= 5; i++) {
+        if (i <= person[0].application_note){
+            displayNote += '<img src="../img/icons/filledStar.svg" alt="logo">';
+        } else {
+            displayNote += '<img src="../img/icons/star.svg" alt="logo">';
+        }
+    }
 
-    $('#profileSpace').append('<img class="form-label d-flex justify-content-center p-3" src="../img/icons/profileBlue.svg" alt="logo" height="150">\n' +
-        '\n' +
-        '  <div class="p-3 mb-3 rounded-3 bg-clearGrey border-0">\n' +
-        '    <span id="nameOutput">'+ person.name + ' ' + person.first_name + '</span>\n' +
+    let nbrMatchPlayed = '';
+    if (person[0].count == null){
+        nbrMatchPlayed = '0 match joué';
+    }
+    if (person[0].count == 1){
+        nbrMatchPlayed = person[0].count + 'match joué';
+    }
+    if (person[0].count > 1){
+        nbrMatchPlayed = person[0].count + 'matchs joués';
+    }
+
+
+    $('#photoProfile').append('<img class="form-label col-4 m-2" src=' + person[0].path + ' alt="logo">\n');
+
+    $('#profileSpace').append(' <div class="p-3 mb-3 rounded-2 bg-clearGrey border-0">\n' +
+        '    <span id="nameOutput">'+ person[0].name + ' ' + person[0].first_name + '</span>\n' +
         '  </div>\n' +
         '\n' +
         '  <div class="p-3 mb-3 rounded-3 border border-clearGrey border-3 align-items-center">\n' +
         '    <img src="../img/icons/map.svg" alt="logo">\n' +
         '    <span class="px-2 text-clearGrey">Ville :</span>\n' +
-        '    <span id="townOutput">' + person.city_name + '</span>\n' +
+        '    <span id="townOutput">' + person[0].name_city + '</span>\n' +
         '  </div>\n' +
         '\n' +
         '  <div class="p-3 mb-3 rounded-3 border border-clearGrey border-3 align-items-center">\n' +
         '    <img src="../img/icons/user.svg" alt="logo">\n' +
         '    <span class="px-2 text-clearGrey">Âge :</span>\n' +
-        '    <span id="ageOutput">XX</span>\n' +
+        '    <span id="ageOutput">'+ person[0].age +'</span>\n' +
         '  </div>\n' +
         '\n' +
         '  <div class="p-3 mb-3 rounded-3 border border-clearGrey border-3 align-items-center">\n' +
         '    <img src="../img/icons/mail.svg" alt="logo">\n' +
         '    <span class="px-2 text-clearGrey">E-mail :</span>\n' +
-        '    <span id="mailOutput">exemple@mail.fr</span>\n' +
+        '    <span id="mailOutput">' + person[0].email + '</span>\n' +
         '  </div>\n' +
         '\n' +
         '  <div class="p-3 mb-3 rounded-3 border border-clearGrey border-3 align-items-center">\n' +
         '    <img src="../img/icons/security-safe.svg" alt="logo">\n' +
         '    <span class="px-2 text-clearGrey">Mot de passe :</span>\n' +
-        '    <span id="passwordOutput">•••••••••••••••</span>\n' +
+        '    <span id="passwordOutput">•••••••••••••</span>\n' +
         '  </div>\n' +
         '\n' +
         '  <div class="p-3 mb-3 rounded-3 border border-clearGrey border-3 align-items-center">\n' +
         '    <img src="../img/icons/user.svg" alt="logo">\n' +
         '    <span class="px-2 text-clearGrey">Forme sportive :</span>\n' +
-        '    <span id="formOutput">...</span>\n' +
+        '    <span id="formOutput">' + person[0].name_form + '</span>\n' +
         '  </div>\n' +
         '\n' +
         '  <div class="p-3 mb-3 rounded-3 bg-clearGrey border-0 d-flex flex-nowrap align-items-center">\n' +
         '    <img src="../img/icons/LogoBisStrong.svg" alt="logo">\n' +
         '    <span class="px-2">Notez l\'application</span>\n' +
-        '    <div id="ratingOutput">\n' +
-        '      <img src="../img/icons/filledStar.svg" alt="logo">\n' +
-        '      <img src="../img/icons/filledStar.svg" alt="logo">\n' +
-        '      <img src="../img/icons/filledStar.svg" alt="logo">\n' +
-        '      <img src="../img/icons/filledStar.svg" alt="logo">\n' +
-        '      <img src="../img/icons/star.svg" alt="logo">\n' +
+        '    <div id="ratingOutput">\n' + displayNote +
         '    </div>\n' +
         '  </div>\n' +
         '\n' +
@@ -91,7 +87,7 @@ function displayPerson(person){
         '\n' +
         '  <div class="p-3 mb-3 rounded-3 bg-clearGrey border-0 align-items-center">\n' +
         '    <img src="../img/icons/badge.svg" alt="logo">\n' +
-        '    <span id="statOutput">23 matchs joués</span>\n' +
+        '    <span id="statOutput">' + nbrMatchPlayed + '</span>\n' +
         '  </div>\n' +
         '\n' +
         '  <div class="align-items-center">\n' +
@@ -107,32 +103,4 @@ function displayPerson(person){
         '      <span>Se déconnecter</span>\n' +
         '    </a>\n' +
         '  </div>');
-
-    ajaxRequest('GET', '../php/libraries/Person/getPerson.php?id_match=' + id_match, displayParticipants);
-}
-
-function displayParticipants(participants){
-    let element;
-    element = document.createElement('div');
-    element.className = 'col-11 bg-clearGrey rounded mx-auto shadow-inset shadow';
-    element2 = document.createElement('h5')
-    element2.className = 'text-black m-2';
-    element2.innerHTML = 'Liste des participants';
-    element.append(element2);
-    /*$('#match').append('<div class="col-11 bg-clearGrey rounded mx-auto shadow-inset shadow">' +
-                        '<h5 class="text-black m-2">Liste des participants</h5>');*/
-    let element3;
-    for (let participant of participants) {
-        element3 = document.createElement('div');
-        element3.className = 'row bg-clearYellow rounded g-2 my-2 shadow';
-        element3.innerHTML = '<div class="col-6 justify-content-between my-auto ps-2">\n' +
-            '<img src="' + participant.path + '" alt="profile" height="50px">\n' +
-            '<span>' + participant.first_name + ' ' + participant.name + '</span>\n' +
-            '</div>\n' +
-            '<div class="col-6 d-flex my-auto pe-2 justify-content-end">\n' +
-            '       ' + participant.count + ' matchs joués\n' +
-            '</div>\n';
-        element.append(element3);
-    }
-    document.getElementById('match2').append(element);
 }
