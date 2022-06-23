7faddd03adb1797c75d60b3a7e5429b410c256e3
Maxence Rogerieux <maxence.rogerieux@isen-ouest.yncrea.fr>
2022-06-23 10:10:50 +0200

    finalisation modifProfile.html et updatePerson.js

--- js/updatePerson.js
+++ js/updatePerson.js
@@ -5,11 +5,11 @@ function displayModifPerson(person){
     if (person[0].application_note != null){
         for (let i = 1; i <= 5; i++) {
             if (i <= person[0].application_note){
-                displayNote += '<div id="star5" onMouseUp="getStarRate(id)" onMouseOver="starFill(id)">\n' +
+                displayNote += '<div id="star' + i + '" onMouseUp="getStarRate(id)" onMouseOver="starFill(id)">\n' +
                     '        <img src="../img/icons/filledStar.svg" alt="logo">\n' +
                     '    </div>';
             } else {
-                displayNote += '<div id="star5" onMouseUp="getStarRate(id)" onMouseOver="starFill(id)">\n' +
+                displayNote += '<div id="star' + i + '" onMouseUp="getStarRate(id)" onMouseOver="starFill(id)">\n' +
                     '        <img src="../img/icons/star.svg" alt="logo">\n' +
                     '    </div>';
             }
@@ -42,24 +42,54 @@ function displayModifPerson(person){
     $('#ratingOutput').append(displayNote);
 
     $('#nameOutput').append(person[0].name + ' ' + person[0].first_name);
-
-    $('#ageOutput').append('<input type="text" class="form-control rounded-3 bg-clearGrey shadow shadow-inset" id="ageInput" placeholder="age" value="' + personAge + '">\n' +
-        '        <label class="ps-4 text-darkGrey" for="ageInput">Entrez votre âge</label>');
 }
 
-$('#updateForm').on('submit', (event) =>
-{
-    let id_photo = $('input[name="avatarInput"]:checked').val();
-    let id_city = $('#townInput').val();
-    let age = $('#ageInput').val();
-    let email = $('#mailInput').val();
-    let password = $('#passwordInput').val();
-    let id_physical_form = $('#formSelect').val();
-    event.preventDefault();
-    ajaxRequest('PUT', '../php/libraries/Person/updatePerson.php',displaySuccess,'id_photo=' + id_photo + '&id_city=' + id_city + '&email=' + email + '&password=' + password + '&age=' + age + '&id_physical_form=' + id_physical_form);
-});
+function submitForm(event){
 
-function displaySuccess(){
+    let id_photo = $('input[name="avatarInput"]:checked').val();
+    let id_city = $('#townOutput').val();
+    let age = $('#ageOutput').val();
+    let password;
+    if ($('#passwordInput').val() == '•••••••••'){
+        password = null
+    } else {
+        password = $('#passwordInput').val();
+    }
+    let id_physical_form = $('#formSelect').val();
+    let application_note = $('#ratingOutput').val();
+    let data = 'id_photo=' + id_photo + '&id_city=' + id_city + '&password=' + password + '&age=' + age + '&id_physical_form=' + id_physical_form + '&application_note=' + application_note;
+    console.log(data);
+    ajaxRequest('PUT', '../php/libraries/Person/updatePerson.php',putSuccess, data);
+}
+
+function putSuccess(){
     console.log("request create success");
     document.location.href="profile.html";
+}
+
+function starFill(id_star){
+
+    var filledStar = "<a href=\"javascript:void(0)\" class=\"cursor-pointer\">\n" +
+        "                    <img src=\"../img/icons/filledStar.svg\" alt=\"logo\">\n" +
+        "                </a>";
+    var star = "<a href=\"javascript:void(0)\" class=\"cursor-pointer\">\n" +
+        "                    <img src=\"../img/icons/star.svg\" alt=\"logo\">\n" +
+        "                </a>";
+
+    let id;
+    for (let i = 1; i <= 5; i++) {
+        if (i <= (id_star.slice(id_star.length - 1))){
+            id = "star" + i;
+            document.getElementById(id).innerHTML = filledStar;
+        } else {
+            id = "star" + i;
+            document.getElementById(id).innerHTML = star;
+        }
+    }
+}
+
+function getStarRate(id_star){
+    let id = id_star;
+    let rate = id.slice(id.length - 1);
+    $('#ratingOutput').val(rate);
 }
